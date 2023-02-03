4ba51cc771b87ff8f53148fea56bba8a32de5565
Seolmiru <drhon10@gmail.com>
2023-02-03 21:29:15 +0900

    플레이어 이동 수정

--- Assets/Yaki'sFolder/Scripts/playermove.cs
+++ Assets/Yaki'sFolder/Scripts/playermove.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermove : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
+    public float maxSpeed;
     Rigidbody2D rigid;
 
     void Awake()
@@ -16,5 +17,10 @@ public class playermove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
 
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1)) // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
     }
 }
