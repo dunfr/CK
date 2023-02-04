890a207901d0794f1ee9dec0a8454e4b7e628992
Seolmiru <drhon10@gmail.com>
2023-02-04 23:14:02 +0900

    jump, camera
    점프시 레이어 충돌 무시, 카메라 플레이어 추적 추가

--- Assets/Yaki'sFolder/Scripts/PlayerController.cs
+++ Assets/Yaki'sFolder/Scripts/PlayerController.cs
@@ -7,35 +7,45 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public float jumpPower;
     Rigidbody2D rigid;
+    int playerLayer, groundLayer;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+
+        playerLayer = LayerMask.NameToLayer("Player");
+        groundLayer = LayerMask.NameToLayer("Ground");
     }
 
     void Update()
     {
-        //jump
+        //Jump
         if (Input.GetButtonDown("Jump"))
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
 
-       //stop speed
+       //Stop Speed
        if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
 
+       //Ignore Layer collision
+        if (rigid.velocity.y > 0)
+            Physics2D.IgnoreLayerCollision(playerLayer, groundLayer, true);
+        else
+            Physics2D.IgnoreLayerCollision(playerLayer, groundLayer, false);
+
     }
 
     void FixedUpdate()
     {
-        //move by controller
+        //Move By Controller
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
-        if (rigid.velocity.x > maxSpeed) //right max speed
+        if (rigid.velocity.x > maxSpeed) //Right Max Speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        else if (rigid.velocity.x < maxSpeed*(-1)) //left max speed
+        else if (rigid.velocity.x < maxSpeed*(-1)) //Left Max Speed
             rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
     }
 }
---
+++ Assets/Yaki'sFolder/Scripts/TrackingCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        //transform.position = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+}
