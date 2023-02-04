73bf2a9cb59b41e021f078b166de25cfde7dba8f
dunfr <j01026708401@gmail.com>
2023-02-04 22:12:58 +0900

    02_04

--- Assets/Pohn'sFolder/Scipts/Ball.cs
+++ Assets/Pohn'sFolder/Scipts/Ball.cs
@@ -4,40 +4,78 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public GameObject Grandfater ;
-    public GameObject Grandmother ;
+    public GameObject Grandfa ;
+    public GameObject Grandma ;
     public float ballSpeed;
-    private float GrandfaterX;
-    private float GrandmotherX;
+    private float GrandfaX;
+    private float GrandmaX;
     private float dist;
     private float nextX;
     private float baseY;
     private float height;
+    private bool isgrandfaThrowing;
+    private bool isgrandmaThrowing;
+
     // Start is called before the first frame update
     void Start()
     {
-        Grandfater = GameObject.Find("Granfater");
-        Grandmother = GameObject.Find("Grandmother");
+        Grandfa = GameObject.Find("Granfater");
+        Grandma = GameObject.Find("Grandmother");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GrandfaterX = Grandfater.transform.position.x;
-        GrandmotherX = Grandmother.transform.position.x;
-        dist = GrandmotherX - GrandfaterX;
-        nextX = Mathf.MoveTowards(transform.position.x, GrandmotherX, ballSpeed * Time.deltaTime);
-        baseY = Mathf.Lerp(Grandfater.transform.position.y, Grandmother.transform.position.y, (nextX - GrandfaterX) / dist);
-        height = 2 * (nextX - GrandfaterX) * (nextX - GrandmotherX) / (-0.25f * dist * dist);
-        Vector3 movePostion = new Vector3(nextX, baseY + height, transform.position.z);
-        transform.rotation = quaternion(movePostion - transform.position);
-        transform.position = movePostion;
-        if(transform.position == Grandmother.transform.position)
+        GrandfaX = Grandfa.transform.position.x;
+        GrandmaX = Grandma.transform.position.x;
+        if(transform.position == Grandma.transform.position)
         {
+            isgrandfaThrowing= true;
+            isgrandmaThrowing= false;
+        }
+        if (transform.position == Grandfa.transform.position)
+        {
+            isgrandmaThrowing = true;
+            isgrandfaThrowing= false;
+        }
+        if (isgrandfaThrowing)
+        {
+            GrandmaThrow();
+        }
+        if (isgrandmaThrowing)
+        {
+            GrandfaThrow();
         }
     }
     public static Quaternion quaternion(Vector2 rotation)
     {
         return Quaternion.Euler(0, 0, Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg);
+    }
+    private void GrandfaThrow()
+    {
+        dist = GrandmaX - GrandfaX;
+        nextX = Mathf.MoveTowards(transform.position.x, GrandmaX, ballSpeed * Time.deltaTime);
+        baseY = Mathf.Lerp(Grandfa.transform.position.y, Grandma.transform.position.y, (nextX - GrandfaX) / dist);
+        height = 2 * (nextX - GrandfaX) * (nextX - GrandmaX) / (-0.25f * dist * dist);
+        Vector3 movePostion = new Vector3(nextX, baseY + height, transform.position.z);
+        transform.rotation = quaternion(movePostion - transform.position);
+        transform.position = movePostion;
+    }
+    private void GrandmaThrow()
+    {
+        dist = GrandfaX - GrandmaX;
+        nextX = Mathf.MoveTowards(transform.position.x, GrandfaX, ballSpeed * Time.deltaTime);
+        baseY = Mathf.Lerp(Grandma.transform.position.y, Grandfa.transform.position.y, (nextX - GrandmaX) / dist);
+        height = 2 * (nextX - GrandfaX) * (nextX - GrandmaX) / (-0.25f * dist * dist);
+        Vector3 movePostion = new Vector3(nextX, baseY + height, transform.position.z);
+        transform.rotation = quaternion(movePostion - transform.position);
+        transform.position = movePostion;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Glove")
+        {
+            Debug.Log("dd");
+        }
     }
 }
---
+++ Assets/Pohn'sFolder/Scipts/GloveManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveManager : MonoBehaviour
+{
+    public BoxCollider2D grandfaGlove;
+    public BoxCollider2D grandmaGlove;
+    private bool isGrandfaGrab;
+    private bool isGrandmaGrab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grandfaGlove.enabled = false;
+        grandmaGlove.enabled = false;
+        isGrandfaGrab = true;
+        isGrandmaGrab = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && isGrandfaGrab)
+        {
+            grandfaGlove.enabled = true;
+            isGrandfaGrab = false;
+            StartCoroutine("GloveTime");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && isGrandmaGrab)
+        {
+            grandmaGlove.enabled = true;
+            isGrandmaGrab = false;
+            StartCoroutine("GloveTime");
+        }
+    }
+    private IEnumerator GloveTime()
+    {   
+        yield return new WaitForSeconds(1f);
+        grandfaGlove.enabled = false;
+        grandmaGlove.enabled = false;
+        isGrandfaGrab = true;
+        isGrandmaGrab = true;
+        StopCoroutine("GloveTime");
+    }
+
+}
