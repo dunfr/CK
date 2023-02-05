28efe7da3863fe7c648417515016bdde3144c927
Seolmiru <drhon10@gmail.com>
2023-02-05 20:40:59 +0900

    Warminigame

---
+++ Assets/Yaki'sFolder/Scripts/Warmini.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warmini : MonoBehaviour
+{
+    public int Index = 0;
+    public int key=9;
+    private KeyCode[] keycodes;
+    private KeyCode[] Randomkey =
+    {
+        KeyCode.DownArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.RightArrow
+    };
+    private void keycodesinit()
+    {
+        keycodes = new KeyCode[key];
+        for (int i = 0; i < keycodes.Length; i++)
+        {
+            keycodes[i] = Randomkey[Random.Range(0, 4)]; 
+        }
+    }
+
+    void Start()
+    {
+        keycodesinit();
+        for (int i = 0; i < keycodes.Length; i++)
+        {
+            Debug.Log(keycodes[i]);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(keycodes[Index]))
+            {
+                Index++;
+                Debug.Log("correct");
+            }
+            else
+            {
+                Index = 0;
+                Debug.Log("Wrong");
+            }
+        }
+    }
+}
